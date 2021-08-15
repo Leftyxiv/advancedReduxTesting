f3e03bec4798191036f04d9c510fe520e0d26a72
Leftyxiv <manny@aledoux.net>
2021-08-15 12:53:45 -0400

    added test for comment list component to render in the app

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,9 +3,14 @@ import { shallow } from 'enzyme';
 
 import App from '../App';
 import CommentBox from '../CommentBox';
-// import CommentList from '../CommentList';
+import CommentList from '../CommentList';
 
 it('shows the comment box comment component', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toBe(1);
+});
+
+it('shows the comment list component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toBe(1);
 });
