9444e18d2ce8abe52901ccaf89b33d066b7d4bae
Leftyxiv <manny@aledoux.net>
2021-08-16 07:43:02 -0400

    added last test for comment box

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,4 +18,18 @@ it('has a text area and a button', () => {
   expect(component.find('button').length).toBe(1);
 });
 
+it('lets users input text into the text area', () => {
+  component.find('textarea').simulate('change', { target: { value: 'Hello' } });
+  component.update();
+  expect(component.find('textarea').prop('value')).toBe('Hello');
+});
+
+it('clears out the text area after the form is submitted', () => {
+  const input = component.find('textarea')
+  input.simulate('change', { target: { value: 'Hello' } });
+  component.find('form').simulate('submit');
+  component.update();
+  expect(input.prop('value')).toBe('');
+});
+
 // console.log(wrapped.find("textarea").length);
