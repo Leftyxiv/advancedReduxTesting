e760cecdeb7b8cfb6180ad0e3a6eb13bb5fdd50d
Leftyxiv <manny@aledoux.net>
2021-08-16 06:09:07 -0400

    refactored test file

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,12 +5,16 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+})
+
 it("shows the comment box comment component", () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toBe(1);
 });
 
 it("shows the comment list component", () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentList).length).toBe(1);
 });
