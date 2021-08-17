60b4ee9d0659a98322ed10897d370944f408ac16
Leftyxiv <manny@aledoux.net>
2021-08-17 07:35:56 -0400

    added a navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,45 @@
 import React from 'react'
 import { Route } from 'react-router';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-const App = () => {
+const App = (props) => {
+  const renderHeader = () => {
+    return <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/post'>Post</Link>
+      </li>
+      <li>
+        { renderButton() }
+      </li>
+    </ul>
+  }
+
+  const renderButton = () => { 
+    if (props.auth) {
+      return <button onClick={() => props.changeAuth(false)}>Logout</button>
+    } else {
+      return <button onClick={() => props.changeAuth(true)}>Login</button>
+    }
+  };
+
   return (
     <div>
+      { renderHeader() }
       <Route exact path="/" component={CommentList}/>
       <Route path='/post' component={CommentBox}/>
     </div>
   )
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return { auth: state.auth }
+}
+export default connect(mapStateToProps, actions)(App);
